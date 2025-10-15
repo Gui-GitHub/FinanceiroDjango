214b34e8522228a4e5f675861ad792f27079bb35
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-10-15 18:54:16 -0300

    Update settings.py

--- FinanceiroDjango/settings.py
+++ FinanceiroDjango/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'chave_develop')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
